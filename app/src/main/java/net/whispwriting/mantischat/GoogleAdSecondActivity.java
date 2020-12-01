e89be778069d71c2cbced3b65e02c9018de378b1
ptbuchanan <42974666+ptbuchanan@users.noreply.github.com>
2020-12-01 16:05:59 -0500

    Added the google files like shown from class

---
+++ app/src/main/java/net/whispwriting/mantischat/GoogleAdSecondActivity.java
@@ -0,0 +1,15 @@
+package net.whispwriting.mantischat;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GoogleAdSecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+}
